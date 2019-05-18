e5c9c1eda09e335570c52b1bc14ffca1e6b82097
Vignesh S <vignesh.s1@photon.in>
2019-05-18 19:12:53 +0530

    Testing public url

--- docs/js.00a46daa.js
+++ docs/js.00a46daa.js
@@ -14080,7 +14080,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63711" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51052" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -14256,4 +14256,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
-//# sourceMappingURL=/js.00a46daa.js.map
+//# sourceMappingURL=js.00a46daa.js.map
